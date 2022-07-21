7b81747f4b942ae0e86572a39144d79e7d314434
dnolan1993 <dnolan1993@yahoo.ie>
2022-07-21 21:16:56 +0000

    ACrrect code for click event for buttons in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,16 @@ document.addEventListener ('DOMContentLoaded', function() {
     let buttons = document.getElementsByTagName('button')
     for(let button of buttons) {
         button.addEventListener('click', function() {
-            if(this.getAttribute('data-type') === 'rock' || 'paper' || 'scissors') {
-                alert ('it works!')
+            if(this.getAttribute('data-type') === 'rock' || this.getAttribute('data-type') === 'paper'
+            || this.getAttribute('data-type') === 'scissors' || this.getAttribute('data-type') === 'john-wick'
+            || this.getAttribute('data-type') === 'chuck-norris'
+            ) {
+                let selection = this.getAttribute('data-type');
+                console.log(selection)
+                alert (`${selection} works!`)
+            } else if(this.getAttribute('data-type') === 'get-started'){
+                
             }
         })
     }
-})
+});
