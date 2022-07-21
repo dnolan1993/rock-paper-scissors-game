6dad394cdfca2cb5dae1e0129f7163e2b3a7ec78
dnolan1993 <dnolan1993@yahoo.ie>
2022-07-21 20:03:17 +0000

    Add click event to buuttons in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 const welcomeMessage = document.getElementById('welcome-message');
 const getStartedBtn = document.getElementById('get-started');
 
-getStartedBtn.addEventListener("click", function() {
+getStartedBtn.addEventListener("click", function () {
     welcomeMessage.style.display = 'none';
 });
+
+document.addEventListener ('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button')
+    for(let button of buttons) {
+        button.addEventListener('click', function() {
+            if(this.getAttribute('data-type') === 'rock' || 'paper' || 'scissors') {
+                alert ('it works!')
+            }
+        })
+    }
+})
